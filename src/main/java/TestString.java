ffa1ec7a5ccdd5e4dcb3ddb9cb9a0a7b191c8393
connor.bae <connor.bae@nsuslab.com>
2022-08-25 16:40:09 +0900

    StringUtils.abbreviate 예제 추가

--- src/main/java/TestString.java
+++ src/main/java/TestString.java
@@ -4,8 +4,18 @@ public class TestString {
 
     public static void main(String[] args) {
 
+        // 최대 길이 - 3 을 초과하는 문자를 ... 으로 변환
         String stringAbbreviate = StringUtils.abbreviate("TestProject_TestString", 15);
-        System.out.println(stringAbbreviate);
+        System.out.println("abbreviate - " + stringAbbreviate);
+
+        // 최대 길이와 sourceText 의 길이가 같으면 sourceText 그대로 반환
+        String stringAbbreviate2 = StringUtils.abbreviate("TestP", 5);
+        System.out.println("abbreviate - " + stringAbbreviate2);
+
+        // 첫문자를 대문자로 변환
+        String stringCapitalize = StringUtils.capitalize("testproject");
+        System.out.println("capitalize - " + stringCapitalize);
+
     }
 
 }
